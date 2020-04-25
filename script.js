c30b06b7d03835a94c9ad5c42768508298703dc1
eneas-hsn <eneashsn@gmail.com>
2020-04-25 19:37:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function reseta() {
+    document.getElementById('text').value='';
+}
+function preencher(valor) {
+    document.getElementById('text').value+=valor;
+}
+
+function calcular() {
+    var resultado=0;
+    resultado=document.getElementById('text').value;
+    //alert(resultado)
+    document.getElementById('text').value='';
+    document.getElementById('text').value=eval(resultado);
+}
+//function apaga() {
+   // document.getElementById('text').value-=preencher();}
