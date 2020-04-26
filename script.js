d1e043379fc68cca0e28171e452ba9751444e8f0
eneas-hsn <eneashsn@gmail.com>
2020-04-26 16:16:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ function reseta() {
 function preencher(valor) {
     document.getElementById('text').value+=valor;
 }
-
 function calcular() {
     var resultado=0;
     resultado=document.getElementById('text').value;
     document.getElementById('text').value=eval(resultado);
 }
    function apagar() {
-    let input = document.getElementById('text');
-    let inputText = text.value;
-    input.value = inputText.substring(0,inputText.length-1);
-    console.log(text.value);
+    text.value = text.value.substring(0,text.value.length-1);
   }
